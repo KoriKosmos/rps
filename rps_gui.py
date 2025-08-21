71dcfef2e32361efddd93dd106f2097e3c051c23
KoriKosmos <90063618+KoriKosmos@users.noreply.github.com>
2025-08-22 00:51:41 +0100

    Refactor button creation for improved readability and consistency in RPS GUI

--- rps_gui.py
+++ rps_gui.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 from rps import rps_logic, normalize_throw, random_throw, load_scores, save_scores
 
+
 class RPSApp:
     def __init__(self, master):
         self.master = master
@@ -15,11 +16,17 @@ class RPSApp:
         self.button_frame = tk.Frame(master)
         self.button_frame.pack()
 
-        self.rock_button = tk.Button(self.button_frame, text="Rock", command=lambda: self.play("rock"))
+        self.rock_button = tk.Button(
+            self.button_frame, text="Rock", command=lambda: self.play("rock")
+        )
         self.rock_button.grid(row=0, column=0)
-        self.paper_button = tk.Button(self.button_frame, text="Paper", command=lambda: self.play("paper"))
+        self.paper_button = tk.Button(
+            self.button_frame, text="Paper", command=lambda: self.play("paper")
+        )
         self.paper_button.grid(row=0, column=1)
-        self.scissors_button = tk.Button(self.button_frame, text="Scissors", command=lambda: self.play("scissors"))
+        self.scissors_button = tk.Button(
+            self.button_frame, text="Scissors", command=lambda: self.play("scissors")
+        )
         self.scissors_button.grid(row=0, column=2)
 
         self.result_label = tk.Label(master, text="")
@@ -42,7 +49,11 @@ class RPSApp:
         t2 = normalize_throw(computer_throw)
         if t1 == t2:
             self.ties += 1
-        elif (t1 == "rock" and t2 == "scissors") or (t1 == "paper" and t2 == "rock") or (t1 == "scissors" and t2 == "paper"):
+        elif (
+            (t1 == "rock" and t2 == "scissors")
+            or (t1 == "paper" and t2 == "rock")
+            or (t1 == "scissors" and t2 == "paper")
+        ):
             self.player_wins += 1
         else:
             self.cpu_wins += 1
@@ -52,6 +63,7 @@ class RPSApp:
     def quit(self):
         save_scores(self.player_wins, self.cpu_wins, self.ties)
         self.master.quit()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
